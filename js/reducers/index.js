7ac89831c83aab2b61fd3f3a615f1d9c70931bfa
caleb tolulope <israel.fabulous@gmail.com>
2017-08-22 10:20:00 +0100

    router reducer added

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
   state: (state = {}) => state,
+  router: routerReducer,
 });
 export default rootReducer;
